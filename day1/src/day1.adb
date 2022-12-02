b15a1dc6a3e9b3a5233caf9d089e41fcf991070c
Mark Wutka <mark.wutka@vanderbilt.edu>
2022-12-02 15:03:20 -0600

    Added overflow checks to day1

--- day1/src/day1.adb
+++ day1/src/day1.adb
@@ -1,31 +1,24 @@
 pragma Spark_Mode (On);
-pragma Overflow_Mode (General =>  Eliminated, Assertions => Eliminated);
 
 with Ada.Text_IO;
 use Ada.Text_IO;
 with Ada.Integer_Text_IO;
 use Ada.Integer_Text_IO;
-with Ada.Command_Line;
-use Ada.Command_Line;
-
 procedure Day1 is
 
    Data_File : File_Type;
-   Group_Sum : Integer;
-   Calories : Integer;
-   Best_3 : array (1 .. 3) of Integer;
+   Group_Sum : Natural;
+   Calories : Natural;
+   FileCal : Integer;
+   Best_3 : array (1 .. 3) of Natural;
 begin
-   if Argument_Count /= 1 then
-      Put_Line ("No input file supplied");
-      return;
-   end if;
 
    Group_Sum := 0;
    Best_3 := (0, 0, 0);
 
    Open (File => Data_File,
          Mode => In_File,
-         Name => Argument (1));
+         Name => "data/day1.txt");
 
    while not End_Of_File (Data_File) loop
       if End_Of_Line (Data_File) then
@@ -41,18 +34,27 @@ begin
          end if;
          Group_Sum := 0;
       else
-         Get (Data_File, Calories);
-         Group_Sum := Group_Sum + Calories;
+         Get (Data_File, FileCal);
+         if FileCal >= 0 then
+            Calories := Natural (FileCal);
+            if Natural'Last - Calories > Group_Sum then
+               Group_Sum := Group_Sum + Calories;
+            end if;
+         end if;
       end if;
       Skip_Line (Data_File);
    end loop;
-
-   Close (Data_File);
 
    Put ("Part A value is ");
    Put_Line (Integer'Image (Best_3 (1)));
 
    Put ("Part B value is ");
-   Put_Line (Integer'Image (Best_3 (1) + Best_3 (2) + Best_3 (3)));
+   if Natural'Last - Best_3 (2) > Best_3 (3)
+      and then Natural'Last - Best_3 (1) > Best_3 (2) + Best_3 (3)
+   then
+      Put_Line (Integer'Image (Best_3 (1) + Best_3 (2) + Best_3 (3)));
+   else
+      Put_Line (" too big.");
+   end if;
 
 end Day1;
